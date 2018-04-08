a16702cf09933ce2ba0da8626b81d4504d4bebfe
aaron <aaron.abm@gmail.com>
2018-04-09 01:36:08 +1000

    Refactored intents to work with new food class

--- app/src/main/java/group1/fitnessapp/dietTracker/EditFoodActivity.java
+++ app/src/main/java/group1/fitnessapp/dietTracker/EditFoodActivity.java
@@ -3,42 +3,60 @@ package group1.fitnessapp.dietTracker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-
-import org.json.JSONException;
 
 import group1.fitnessapp.R;
 
 public class EditFoodActivity extends AppCompatActivity {
     private Food originalFood = null;
-    private Food returnFood = null;
+    private Food editedFood = null;
+
+    private TextView name = null;
+    private TextView subText = null;
+    private TextView calories = null;
+    private EditText servings = null;
+    private TextView servingSize = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_food);
 
-        String name = getIntent().getStringExtra("selectedFoodName");
-        String subText = getIntent().getStringExtra("selectedFoodSubText");
-        double servings = getIntent().getDoubleExtra("selectedFoodServings", 1);
-        double servingQuantity = getIntent().getDoubleExtra("selectedFoodServingQuantity", 0);
-        String servingUnit = getIntent().getStringExtra("selectedFoodServingUnit");
-        double calories = getIntent().getDoubleExtra("selectedFoodCalories", 999999);
-        String JSON = getIntent().getStringExtra("selectedFoodJSON");
-        try {
-            originalFood = new Food(name, subText, servings, servingQuantity, servingUnit, calories, JSON);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        name = findViewById(R.id.addFoodName);
+        subText = findViewById(R.id.addFoodSubText);
+        calories = findViewById(R.id.displayFoodCalories);
+        servings = findViewById(R.id.editServings);
+        servingSize = findViewById(R.id.addFoodServingsQuantity);
+        originalFood = (Food) getIntent().getSerializableExtra("foodToEdit");
+        editedFood = new Food(originalFood);
+        populateFields();
 
-        if(originalFood != null){
-            populateFields();
-        }
+        // Edit text fields update logic
+        servings.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-        //All the buttons and there functions
-        ImageButton back = findViewById(R.id.btnBack);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                updateFields();
+            }
+        });
+
+
+        // All the buttons and there functions
+        ImageButton back = findViewById(R.id.addBtnBack);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,7 +76,7 @@ public class EditFoodActivity extends AppCompatActivity {
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                deleteFood();
+                deleteFood(originalFood);
             }
         });
     }
@@ -66,27 +84,37 @@ public class EditFoodActivity extends AppCompatActivity {
     private void saveFood() {
         Intent intent = new Intent();
         intent.putExtra("actionCode", 1);
+        intent.putExtra("editSaveFoodOriginal", originalFood);
+        intent.putExtra("editSaveFoodNew", editedFood);
         setResult(RESULT_OK, intent);
         finish();
     }
 
-    private void deleteFood() {
+    private void deleteFood(Food food) {
         Intent intent = new Intent();
         intent.putExtra("actionCode", 0);
-        intent.putExtra("foodName", originalFood.getName());
-        intent.putExtra("foodSubText", originalFood.getSubText());
-        intent.putExtra("foodServingQuantity", originalFood.getServingQuantity());
-        intent.putExtra("foodServingUnit", originalFood.getServingUnit());
-        intent.putExtra("foodCalories", originalFood.getCalories());
-        intent.putExtra("foodOriginalJSON", String.valueOf(originalFood.getOriginalJSON()));
+        intent.putExtra("editDeleteFood", food);
         setResult(RESULT_OK, intent);
         finish();
     }
 
     private void populateFields() {
-        TextView name = findViewById(R.id.displayFoodName);
-        TextView subText = findViewById(R.id.displayFoodSubText);
         name.setText(originalFood.getName());
         subText.setText(originalFood.getSubText());
+        calories.setText(Integer.toString((int) originalFood.getTotalCalories()));
+        servings.setText(Double.toString(originalFood.getServings()));
+        String servingQuantity = originalFood.getServingQuantity() +" " +originalFood.getServingUnit();
+        servingSize.setText(servingQuantity);
+    }
+
+    private void updateFields(){
+        try{
+            double newServings = Double.parseDouble(servings.getText().toString());
+            editedFood = editedFood.editServings(newServings);
+            calories.setText(Integer.toString((int)editedFood.getTotalCalories()));
+        }catch (NumberFormatException e){
+            System.out.println(e);
+            calories.setText("0");
+        }
     }
 }
